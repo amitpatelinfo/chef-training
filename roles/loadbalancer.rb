bd4d4cd839a7940945fbec042711f38545efa307
Amit Patel <amit.patel@synerzip.com>
2014-02-26 08:52:49 +0530

    Adding Loadbancer cookbook

--- roles/loadbalancer.rb
+++ roles/loadbalancer.rb
@@ -1,10 +1,15 @@
 name 		"loadbalancer"
 description "Load Balancer role"
 run_list(
- "recipe[base]"
+ "recipe[base]",
+ "recipe[chef-elb]"
 )
-override_attributes(
+default_attributes(
  "apache" => {
  	"port" => "80"
+ },
+ "aws"	=> {
+ 	"accessid" => "AKIAIVPZ24QNRPITTQQA",
+ 	"secretkey" => "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"
  }
 )
--- roles/webserver.rb
+++ roles/webserver.rb
@@ -3,12 +3,9 @@ description	"Role webserver"
 run_list(
 	"recipe[base]",
 	"recipe[tomcat]",
-	"recipe[chef-client]"
+	"recipe[helloservice]"
 )
 override_attributes(
- "chef_client" => {
- 	"interval" => "10"
- },	
  "tomcat" => {
  	"base_version" => "7",
  	"port" => "8080"
